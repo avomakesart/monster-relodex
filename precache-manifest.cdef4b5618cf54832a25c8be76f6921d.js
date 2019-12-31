4b94ea52d367a2e8e54c629532e9d094ddaa8b20
Alvaro Castillo Correa <alvaro.castillo777@gmail.com>
2019-12-31 10:11:56 -0600

    Updates

---
+++ precache-manifest.cdef4b5618cf54832a25c8be76f6921d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d60f8750e65ab975388d65104b02690",
+    "url": "/monster-relodex/index.html"
+  },
+  {
+    "revision": "3d17807bdda134b6f0da",
+    "url": "/monster-relodex/static/css/main.9810f72f.chunk.css"
+  },
+  {
+    "revision": "edf5e20ca71e1243abd3",
+    "url": "/monster-relodex/static/js/2.db871858.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/monster-relodex/static/js/2.db871858.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d17807bdda134b6f0da",
+    "url": "/monster-relodex/static/js/main.16626cbe.chunk.js"
+  },
+  {
+    "revision": "fe2839f7c754784762e2",
+    "url": "/monster-relodex/static/js/runtime-main.a6b6b769.js"
+  }
+]);
